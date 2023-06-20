770499552f61a794edf32ad6ca3b6d0e5436b26d
Yuvaraj Rai <44267854+yuvarajrai1@users.noreply.github.com>
2023-06-19 20:05:47 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,18 +1,18 @@
 <?php
-    $host = 'yuvaraj-mysqlserver.mysql.database.azure.com';
-    $username = 'yuvaraj';
-    $password = 'Brooklyn44144';
-    $database = 'autoinventory_db';
+    // $host = 'yuvaraj-mysqlserver.mysql.database.azure.com';
+    // $username = 'yuvaraj';
+    // $password = 'Brooklyn44144';
+    // $database = 'autoinventory_db';
 
-    $conn = mysqli_init();
-    mysqli_ssl_set($conn, NULL, NULL, "/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    mysqli_real_connect($conn, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
+    // $conn = mysqli_init();
+    // mysqli_ssl_set($conn, NULL, NULL, "/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    // mysqli_real_connect($conn, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
 
-    if(mysqli_connect_errno()){
-        die("Failed to conect to MySQL: ".mysqli_connect_error());
-    }
-    else{
-        echo("<script>console.log('connection successfull')</script>");
-    }
+    // if(mysqli_connect_errno()){
+    //     die("Failed to conect to MySQL: ".mysqli_connect_error());
+    // }
+    // else{
+    //     echo("<script>console.log('connection successfull')</script>");
+    // }
 
 ?>
